f2e8c03d30f83938eab3e5b3f2c8839bd9ba797c
Pavani <136723974+NerusuPavani37@users.noreply.github.com>
2025-05-17 19:39:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 const makeChange = (c) => {
   // your name here
+	 let quater = 25;
+  let dimens = 10;
+  let nickles = 5;
+  let penny = 1;
+
+  let coins = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0
+  };
+
+  if (c >= quater) {
+    coins.q = Math.floor(c / quater);
+    c = c % quater;
+  }
+
+  if (c >= dimens) {
+    coins.d = Math.floor(c / dimens);
+    c = c % dimens;
+  }
+
+  if (c >= nickles) {
+    coins.n = Math.floor(c / nickles);
+    c = c % nickles;
+  }
+
+  if (c >= penny) {
+    coins.p = Math.floor(c / penny);
+    c = c % penny;
+  }
+
+  return coins;
 };
 
 // Do not the change the code below
